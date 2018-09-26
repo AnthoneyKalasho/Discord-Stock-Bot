d6f9fa0173ef0f268df420cd2422c55f8d39c198
AnthoneyKalasho <anthoneykalasho@gmail.com>
2018-09-26 14:10:51 -0700

    Fixed times

--- stockBot.py
+++ stockBot.py
@@ -14,75 +14,75 @@ pattern = re.compile(r'\$([^\s]+)')
 @client.event
 async def on_message(message):
     now = datetime.now()
-
-    if message.author == client.user:
-        return
-    for m in re.findall(pattern, message.content):
-        if(len(m) > 5 or not m.isalpha()):
+    if (now.weekday() < 5) and (5 <= now.hour <= 15):
+        if message.author == client.user:
             return
-        else:
-            page = requests.get('https://api.iextrading.com/1.0/stock/'
-                                + m + '/quote')
-            json_string = json.loads(page.text)
-            symbol = json_string["symbol"]
-            companyName = json_string["companyName"]
-            marketPercent = round(json_string["changePercent"] * 100, 3)
-            marketPercentString = str(marketPercent)
-            latestPrice = json_string["latestPrice"]
-            change = round(json_string["change"], 3)
+        for m in re.findall(pattern, message.content):
+            if(len(m) > 5 or not m.isalpha()):
+                return
+            else:
+                page = requests.get('https://api.iextrading.com/1.0/stock/'
+                                    + m + '/quote')
+                json_string = json.loads(page.text)
+                symbol = json_string["symbol"]
+                companyName = json_string["companyName"]
+                marketPercent = round(json_string["changePercent"] * 100, 3)
+                marketPercentString = str(marketPercent)
+                latestPrice = json_string["latestPrice"]
+                change = round(json_string["change"], 3)
+                extendedHours = False
 
-            if now.hour > 13 or now.hour < 5:
-                extendedPrice = json_string["extendedPrice"]
-                extendedChange = json_string["extendedChange"]
-                extendedChangePercent = json_string["extendedChangePercent"]
-                if extendedChangePercent >= 0:
-                    positive = "+"
+                if ((5 <= (now.hour + (now.minute/60)) <= 6.5) or (13 <= (now.hour + (now.minute/60)) <= 15):
+                    extendedPrice = json_string["extendedPrice"]
+                    extendedChange = json_string["extendedChange"]
+                    extendedChangePercent = json_string["extendedChangePercent"]
+                    if extendedChangePercent >= 0:
+                        positive = "+"
+                    else:
+                        positive = ""
+
+                if (5 <= (now.hour + (now.minute/60)) <= 6.5):
+                    marketTime = "PM"
+                    extendedHours = True
+                elif (13 <= (now.hour + (now.minute/60)) <= 15):
+                    marketTime = "AH"
+                    extendedHours = True
+
+                if marketPercent >= 0:
+                    marketPercentString = "+" + str(marketPercent) + "%"
                 else:
-                    positive = ""
+                    marketPercentString = str(marketPercent) + "%"
 
-            if 630 > (now.hour + now.minute) > 5:
-                marketTime = "PM"
-                extendedHours = True
+                if change >= 0:
+                    changeString = "+" + str(change)
+                else:
+                    changeString = str(change)
 
-            if (now.hour + now.minute) > 13 or (now.hour + now.minute) < 5 or now.day > 4:
-                marketTime = "AH"
-                extendedHours = True
-
-            if marketPercent >= 0:
-                marketPercentString = "+" + str(marketPercent) + "%"
-            else:
-                marketPercentString = str(marketPercent) + "%"
-
-            if change >= 0:
-                changeString = "+" + str(change)
-            else:
-                changeString = str(change)
-
-            if extendedHours:
-                embed = discord.Embed(title="" + companyName +
-                                      " ($" + symbol + ")",
-                                      url="https://www.tradingview.com/symbols/"
-                                      + symbol,
-                                      description=str(round(latestPrice, 3))
-                                      + " -> (" + changeString
-                                      + " / " + marketPercentString
-                                      + ")" + " | "
-                                      + marketTime + " "
-                                      + str(round(extendedPrice, 3))
-                                      + " -> (" + positive
-                                      + str(extendedChange)
-                                      + " / " + positive + str(round(extendedChangePercent, 2)) + "%"
-                                      + ")", color=0x006BB6)
-            else:
-                embed = discord.Embed(title=""
-                                      + companyName
-                                      + " ($" + symbol + ")",
-                                      url="https://www.tradingview.com/symbols/"
-                                      + symbol,
-                                      description=str(round(latestPrice, 3)) +
-                                      " -> (" + changeString
-                                      + " / " + marketPercentString
-                                      + ")", color=0x006BB6)
-            await client.send_message(message.channel, embed=embed)
+                if extendedHours:
+                    embed = discord.Embed(title="" + companyName +
+                                          " ($" + symbol + ")",
+                                          url="https://www.tradingview.com/symbols/"
+                                          + symbol,
+                                          description=str(round(latestPrice, 3))
+                                          + " -> (" + changeString
+                                          + " / " + marketPercentString
+                                          + ")" + " | "
+                                          + marketTime + " "
+                                          + str(round(extendedPrice, 3))
+                                          + " -> (" + positive
+                                          + str(extendedChange)
+                                          + " / " + positive + str(round(extendedChangePercent, 2)) + "%"
+                                          + ")", color=0x006BB6)
+                else:
+                    embed = discord.Embed(title=""
+                                          + companyName
+                                          + " ($" + symbol + ")",
+                                          url="https://www.tradingview.com/symbols/"
+                                          + symbol,
+                                          description=str(round(latestPrice, 3)) +
+                                          " -> (" + changeString
+                                          + " / " + marketPercentString
+                                          + ")", color=0x006BB6)
+                await client.send_message(message.channel, embed=embed)
 
 client.run(TOKEN)
